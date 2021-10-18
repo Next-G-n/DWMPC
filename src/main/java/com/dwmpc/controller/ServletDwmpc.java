231a02ecce71a4908671e581da582b12a0d462ac
danchoba <danielchobakaone@gmail.com>
2021-10-18 16:06:38 +0200

    added the twilio email method just near the twilio sms

--- src/main/java/com/dwmpc/controller/ServletDwmpc.java
+++ src/main/java/com/dwmpc/controller/ServletDwmpc.java
@@ -3,6 +3,13 @@ import javax.servlet.annotation.MultipartConfig;
 
 import com.dwmpc.model.DAO.ConnectionUtil;
 import com.dwmpc.model.bean.*;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -158,6 +165,27 @@ public class ServletDwmpc extends HttpServlet {
         catch (Exception exc) {
             exc.printStackTrace();
 
+        }
+    }
+    private void twilioemail()throws Exception{
+        Email from = new Email("danielchobakaone@gmail.com");
+        String subject = "Sending with SendGrid is Fun";
+        Email to = new Email("Nshathisismile@gmail.com");
+        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid("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");
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
         }
     }
 
